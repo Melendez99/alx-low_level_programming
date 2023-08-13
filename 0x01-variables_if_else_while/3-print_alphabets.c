be62c182ca38c6f190505de3e9847d30760b85a3
Melody George <melodygeorge99@gmail.com>
2023-08-13 09:07:23 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,12 +5,13 @@
  *
  * Return: returns 0
  */
-int main(void)
-{   
-	char i;
 
-	for (i = 'a' ; i <= 'z' ; i++)
-		putchar(i);
-	putchar('\n');
-	return (0);
+int main(void)
+{
+char i;
+
+for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+putchar('\n');
+return (0);
 }
