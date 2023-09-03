197507109ccac8e8ff5ac0bc745e4b6c67374274
Melody George <melodygeorge99@gmail.com>
2023-09-03 03:18:42 -0700

    task 0

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,1 +1,18 @@
-char *_memcpy(char *dest, char *src, unsigned int n);
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destined location
+ * @src: source location
+ * @n: number of bytes to copy
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0 ; i < n ; i++)
+                dest[i] = src[i];
+        return (dest);
+}
--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,1 +1,24 @@
-char *_strpbrk(char *s, char *accept);
+#include "main.h"
+
+/**
+ * _strpbrk - locates first occurrence in string
+ * @s: input
+ * @accept: input
+ * Return: 0 successfully
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+        int i;
+
+        while (*s)
+        {
+                for (i = 0 ; accept[i]; i++)
+                {
+                        if (*s == accept[i])
+                                return (s);
+                }
+                s++;
+        }
+        return ('\0');
+}
