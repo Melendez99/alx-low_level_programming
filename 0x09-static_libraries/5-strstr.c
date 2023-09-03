c62ea2a0e40c0ebdcfa608ca3dce943304ddb7cd
Melody George <melodygeorge99@gmail.com>
2023-09-03 02:35:19 -0700

    task 0

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,3 +1,28 @@
 #include "main.h"
 
-char *_strstr(char *haystack, char *needle);
+/**
+ * _strstr - finds occurence of a substring
+ * @haystack: string
+ * @needle: substring
+ * Return: 0 always
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+        for (; *haystack != '\0'; haystack++)
+        {
+                char *i = haystack;
+                char *p = needle;
+
+                while (*i == *p && *p != '\0')
+                {
+                        i++;
+                        p++;
+                }
+
+                if (*p == '\0')
+                        return (haystack);
+        }
+        return (0);
+}
+
