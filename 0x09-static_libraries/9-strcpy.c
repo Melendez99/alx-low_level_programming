a876b03c3ca522596f31415333c5a75ff5e3073c
Melody George <melodygeorge99@gmail.com>
2023-09-05 05:14:45 -0700

    task1

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,1 +1,25 @@
-char *_strcpy(char *dest, char *src);
+#include "main.h"
+
+/**
+ * _strcpy - function copies string
+ * @dest: pointer
+ * @char: pointer
+ * Return: 0 always
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int m = 0;
+	int i = 0;
+
+	while (*(src + 1) != '\0')
+	{
+		m++;
+	}
+	for ( ; i < m ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[m] = '\0';
+	return (dest);
+}
