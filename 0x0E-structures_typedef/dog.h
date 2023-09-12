f10b12ae36087b6d9ee575b587f5bb89d02f38df
Melody George <melodygeorge99@gmail.com>
2023-09-11 21:44:21 -0700

    task 3 dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
-#include <stdio.h>
-#include <stdlib.h>
+
 
 /**
  * struct dog - new type
@@ -17,9 +16,17 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - new type(typedef) for strut dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
