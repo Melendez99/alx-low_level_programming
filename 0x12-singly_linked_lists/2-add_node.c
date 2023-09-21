248736c5b6f5b00b0f0cd1ffe85d7b628fb2ad7b
Melody George <melodygeorge99@gmail.com>
2023-09-21 11:04:50 -0700

    task 2 mod

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,9 +6,9 @@
  * add_node - adds a new node at the beginning of a linked
  * @head: a double pointer to linked lists
  * @str: new string
- *
  * Return: address of the new element, or NULL upon failure
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *n;
@@ -24,5 +24,8 @@ list_t *add_node(list_t **head, const char *str)
 	n->str = strdup(str);
 	n->len = len;
 	n->next = (*head);
+	(*head) = n;
+
+	return (*head);
 
 }
