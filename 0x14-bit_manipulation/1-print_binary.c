47a24e07837e21c8e78ccb26d603b3c161ed4ef9
Melendez99 <melodygeorge99@gmail.com>
2023-10-26 19:44:05 -0700

    binary rep of a num task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_binary - prints binary representation of a number
+ * @n: number to print as binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int binary;
+	int i;
+
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+
+	for (binary = n, i = 0; (temp >>= 1) > 0; i++)
+		;
+
+	for (; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
